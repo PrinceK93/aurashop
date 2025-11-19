197a93c93acb9edb0e37da3ba1763763d366fa90
PrinceK93 <princed55x@gmail.com>
2025-11-19 23:24:21 +0530

    route

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -25,6 +25,6 @@ export async function GET(request: NextRequest) {
     }
   }
 
-  // redirect the user to an error page with some instructions
+  
   redirect(`/auth/error?error=No token hash or type`);
 }
