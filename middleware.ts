03846b420090fc75734b8af05d3e378f6ca89bb9
PrinceK93 <140707024+PrinceK93@users.noreply.github.com>
2025-12-27 19:49:20 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { type NextRequest } from "next/server";
 export async function middleware(request: NextRequest) {
   return await updateSession(request);
 }
-
+ 
 export const config = {
   matcher: [ 
     /*  
