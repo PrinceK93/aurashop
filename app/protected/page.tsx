7799cbca27f321b7eb1eb584092c83334b8478cd
PrinceK93 <140707024+PrinceK93@users.noreply.github.com>
2026-01-02 12:55:13 +0530

    Update page.tsx

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -6,7 +6,7 @@ import { FetchDataSteps } from "@/components/tutorial/fetch-data-steps";
  
 export default async function ProtectedPage() { 
   const supabase = await createClient(); 
-
+ 
   const { data, error } = await supabase.auth.getClaims();
   if (error || !data?.claims) { 
     redirect("/auth/login");
