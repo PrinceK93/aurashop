323f6b502919a0e383a4dd28e0ef5fdfe1bdc766
PrinceK93 <140707024+PrinceK93@users.noreply.github.com>
2026-01-02 12:55:08 +0530

    Update page.tsx

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -5,7 +5,7 @@ import { InfoIcon } from "lucide-react";
 import { FetchDataSteps } from "@/components/tutorial/fetch-data-steps";
  
 export default async function ProtectedPage() { 
-  const supabase = await createClient();
+  const supabase = await createClient(); 
 
   const { data, error } = await supabase.auth.getClaims();
   if (error || !data?.claims) { 
